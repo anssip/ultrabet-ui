a9c1db51cfd3370ae5aa5949e40c9300d4fe455f
Anssi Piirainen <api@iki.fi>
2023-11-03 17:27:14 +0200

    Add long bet option

--- src/ui/bet-slip/place-bet-form.tsx
+++ src/ui/bet-slip/place-bet-form.tsx
@@ -8,6 +8,7 @@ import styles from '@/ui/bet-slip/bet-slip.module.css'
 import globals from '@/ui/globals.module.css'
 import React from 'react'
 import { Slip } from '@/ui/bet-slip/bet-slip'
+import { MarketOption } from '@/gql/types.generated'
 
 type Props = {
   slip: Slip
@@ -30,42 +31,70 @@ const initialState = {
   message: null,
 }
 
+function getLongBetName(length: number) {
+  const names = new Map([
+    [2, 'Double'],
+    [3, 'Treble'],
+    [4, 'Fourfold'],
+    [5, 'Fivefold'],
+    [6, 'Sixfold'],
+    [7, 'Sevenfold'],
+    [8, 'Eightfold'],
+    [9, 'Ninefold'],
+    [10, 'Tenfold'],
+  ])
+  return names.get(length) ?? `${length}-fold`
+}
+
 export function PlaceBetForm({ slip }: Props) {
   const { user } = useUser()
   const [betFormState, betFormAction] = useFormState(
     placeBet.bind(null, user ?? null, slip),
     initialState
   )
+  const options = [...Object.keys(slip)]
+
+  function renderOption(
+    option: MarketOption & { stake?: number; marketName: string; eventName: string }
+  ) {
+    return (
+      <li key={option.id} className={styles.option}>
+        <div className={styles.header}>
+          <div className={styles.name}>
+            <div>{option.name}</div>
+            <div>{option.odds.toFixed(2)}</div>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div>{option.marketName}</div>
+          <div>{option.eventName}</div>
+        </div>
+        <input
+          className={styles.stake}
+          type="number"
+          name={`${option.id}-stake`}
+          id={`${option.id}-stake`}
+          min={0}
+          step={0.01}
+          placeholder="Enter stake"
+          required
+        />
+      </li>
+    )
+  }
 
   return (
-    <form action={betFormAction}>
+    <form action={betFormAction} className={styles.betForm}>
       <ol className={`${styles.right} ${styles.column}`}>
-        {Object.keys(slip)
-          .map((optionIdStr) => slip[optionIdStr])
-          .map((option) => (
-            <li key={option.id} className={styles.option}>
-              <div className={styles.header}>
-                <div className={styles.name}>
-                  <div>{option.name}</div>
-                  <div>{option.odds}</div>
-                </div>
-              </div>
-              <div className={styles.content}>
-                <div>{option.marketName}</div>
-                <div>{option.eventName}</div>
-              </div>
-              <input
-                className={styles.stake}
-                type="number"
-                name={`${option.id}-stake`}
-                id={`${option.id}-stake`}
-                min={0}
-                step={0.01}
-                placeholder="Enter stake"
-                required
-              />
-            </li>
-          ))}
+        {options.map((optionIdStr) => slip[optionIdStr]).map(renderOption)}
+        {options.length > 1 &&
+          renderOption({
+            odds: options.map((optionId) => slip[optionId]).reduce((acc, o) => acc * o.odds, 1),
+            marketName: '',
+            eventName: '',
+            id: 'long',
+            name: getLongBetName(options.length),
+          })}
       </ol>
 
       <div className={styles.actions}>
