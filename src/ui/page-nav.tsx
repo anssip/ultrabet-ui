58a0aa6ab2cb5e6260c882cbe31339f29dd60de1
Anssi Piirainen <api@iki.fi>
2024-02-09 08:50:09 +0200

    Fix navigation active state for 'h2h' slug

--- src/ui/page-nav.tsx
+++ src/ui/page-nav.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 function NavLink({ label, slug }: { label: string; slug: string }) {
   const path = usePathname()
   console.log('segment', path)
-  const isActive = path.includes(slug)
+  const isActive = path.includes(slug) || (slug === 'h2h' && path === '/')
 
   return (
     <Link className={`${isActive ? styles.active : ''}`} href={slug}>
